aa46ca19b1fe1d96f2e7468191aa092795a7741f
sarkis-k <sargis.leva@gmail.com>
2022-03-23 14:44:47 -0700

    implemented the turtle and moving sequence

--- 23_road_cross/player.py
+++ 23_road_cross/player.py
@@ -1,7 +1,17 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.setheading(90)
+        self.penup()
+        self.shape("turtle")
+        self.goto(0, -280)
+
+    def up_move(self):
+        self.forward(10)
