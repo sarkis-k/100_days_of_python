6db4d38720afd704c1f106d3e785771e14a2ec3f
sarkis-k <sargis.leva@gmail.com>
2022-04-08 11:27:08 -0700

    csv manip using pandas messing with squirrel data

--- 25_weather_csv/main.py
+++ 25_weather_csv/main.py
@@ -15,5 +15,33 @@
 
 import pandas as pd
 
-data = pd.read_csv("weather_data.csv")
-print(data)
+# data = pd.read_csv("weather_data.csv")
+# # print(data)
+# # print(data[data.temp == data["temp"].max()])
+
+data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+# print(data["Primary Fur Color"])
+
+# # worked as intended but complexity is O(n) x2
+# data_dict = {
+# }
+#
+# for x in data["Primary Fur Color"]:
+#     data_dict[x]=0
+#
+# for x in data["Primary Fur Color"]:
+#     data_dict[x] +=1
+
+gray_squirrels = len(data[data["Primary Fur Color"] == "Gray"])
+cinnamon_squirrels = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels = len(data[data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+    "Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels, cinnamon_squirrels, black_squirrels]
+}
+
+df = pd.DataFrame(data_dict)
+df.to_csv("color count.csv")
+
+print(df)
