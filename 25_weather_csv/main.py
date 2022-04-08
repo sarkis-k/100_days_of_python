3e4dae3df7b245086ce5d698eb24db1b98fe4afa
sarkis-k <sargis.leva@gmail.com>
2022-04-07 17:01:50 -0700

    csv manip using pandas

--- 25_weather_csv/main.py
+++ 25_weather_csv/main.py
@@ -3,12 +3,17 @@
 #
 # print(data)
 
-import csv
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temp_list = []
+#     for row in data:
+#         if row[1]!="temp":
+#             temp_list.append(int(row[1]))
+#     print(temp_list)
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temp_list = []
-    for row in data:
-        if row[1]!="temp":
-            temp_list.append(int(row[1]))
-    print(temp_list)
+import pandas as pd
+
+data = pd.read_csv("weather_data.csv")
+print(data)
