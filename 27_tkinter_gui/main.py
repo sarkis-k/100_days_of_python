8df3b638c2fc760d5fca90b434efc3a8514f7467
sarkis-k <sargis.leva@gmail.com>
2022-04-11 18:20:16 -0700

    tkinter GUI miles to km converter completed

--- 27_tkinter_gui/main.py
+++ 27_tkinter_gui/main.py
@@ -1,37 +1,137 @@
 import tkinter
 
 
-def add(*args):
-    sum = 0
-    for x in args:
-        sum += x
-    print(sum)
-
 def button_clicked():
     # print("clicked")
-    new_t = tkinput.get()
-    my_label.config(text=new_t)
-
+    new_t = int(input_m.get())
+    calc=1.6*new_t
+    output_label.config(text=str(calc))
+    print(calc)
 
 
 window = tkinter.Tk()
-window.title("My GUI")
+window.title("Mile to Km Converter")
 window.minsize(width=500, height=300)
 
-
 # label
-my_label = tkinter.Label(text="hello", font=("Arial, 24"))
-my_label.pack()
+m_label = tkinter.Label(text="miles")
+m_label.grid(column=3, row=0)
+t_label = tkinter.Label(text="is equal to")
+t_label.grid(column=0, row=1)
+k_label = tkinter.Label(text="km")
+k_label.grid(column=3, row=1)
 
-add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+output_label = tkinter.Label(text="0")
+output_label.grid(column=1, row=1)
 
 
-button = tkinter.Button(text="CLick me", command=button_clicked)
-button.pack()
+button = tkinter.Button(text="Convert", command=button_clicked)
+button.grid(column=3,row=2)
 
-tkinput = tkinter.Entry()
-tkinput.pack()
-
+input_m = tkinter.Entry()
+input_m.grid(column=2, row=0)
 
 
 window.mainloop()
+
+#
+# from tkinter import *
+#
+# # Creating a new window and configurations
+# window = Tk()
+# window.title("Widget Examples")
+# window.minsize(width=500, height=500)
+#
+# # Labels
+# label = Label(text="This is old text")
+# label.config(text="This is new text")
+# label.pack()
+#
+#
+# # Buttons
+# def action():
+#     print("Do something")
+#
+#
+# # calls action() when pressed
+# button = Button(text="Click Me", command=action)
+# button.pack()
+#
+# # Entries
+# entry = Entry(width=30)
+# # Add some text to begin with
+# entry.insert(END, string="Some text to begin with.")
+# # Gets text in entry
+# print(entry.get())
+# entry.pack()
+#
+# # Text
+# text = Text(height=5, width=30)
+# # Puts cursor in textbox.
+# text.focus()
+# # Adds some text to begin with.
+# text.insert(END, "Example of multi-line text entry.")
+# # Gets current value in textbox at line 1, character 0
+# print(text.get("1.0", END))
+# text.pack()
+#
+#
+# # Spinbox
+# def spinbox_used():
+#     # gets the current value in spinbox.
+#     print(spinbox.get())
+#
+#
+# spinbox = Spinbox(from_=0, to=10, width=5, command=spinbox_used)
+# spinbox.pack()
+#
+#
+# # Scale
+# # Called with current scale value.
+# def scale_used(value):
+#     print(value)
+#
+#
+# scale = Scale(from_=0, to=100, command=scale_used)
+# scale.pack()
+#
+#
+# # Checkbutton
+# def checkbutton_used():
+#     # Prints 1 if On button checked, otherwise 0.
+#     print(checked_state.get())
+#
+#
+# # variable to hold on to checked state, 0 is off, 1 is on.
+# checked_state = IntVar()
+# checkbutton = Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
+# checked_state.get()
+# checkbutton.pack()
+#
+#
+# # Radiobutton
+# def radio_used():
+#     print(radio_state.get())
+#
+#
+# # Variable to hold on to which radio button value is checked.
+# radio_state = IntVar()
+# radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
+# radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+# radiobutton1.pack()
+# radiobutton2.pack()
+#
+#
+# # Listbox
+# def listbox_used(event):
+#     # Gets current selection from listbox
+#     print(listbox.get(listbox.curselection()))
+#
+#
+# listbox = Listbox(height=4)
+# fruits = ["Apple", "Pear", "Orange", "Banana"]
+# for item in fruits:
+#     listbox.insert(fruits.index(item), item)
+# listbox.bind("<<ListboxSelect>>", listbox_used)
+# listbox.pack()
+# window.mainloop()
