ebd629cae8c4b87a5c1563a52c4c5fa7ceac8e48
sarkis-k <sargis.leva@gmail.com>
2022-04-14 21:29:54 -0700

    implemented data entry algo and random pswd generator

--- 29_pswd_generator/main.py
+++ 29_pswd_generator/main.py
@@ -1,8 +1,45 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+def passwd_gen():
+    passwd_ent.delete(0, END)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = []
+    password_list = [random.choice(letters) for char in range(random.randint(8, 10))]
+    password_list += [random.choice(symbols) for sym in range(random.randint(2, 4))]
+    password_list += [random.choice(numbers) for num in range(random.randint(2, 4))]
+    random.shuffle(password_list)
+    password = "".join(password_list)
+
+    passwd_ent.insert(0, password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def add_entry():
+    if web_ent.get() == "" or passwd_ent.get() == "":
+        messagebox.showinfo(title="Wrong Entry", message="Can't be empty")
+    else:
+        is_ok = messagebox.askokcancel(title=web_ent.get(), message=f"These are the details entered: \n"
+                                                                    f"Email: {user_ent.get()}\n"
+                                                                    f"Password: {passwd_ent.get()}\n"
+                                                                    f"Is it ok to save?")
+        if is_ok:
+            with open(file="data.txt", mode="a") as file:
+                file.write(f"{web_ent.get()} | {user_ent.get()} | {passwd_ent.get()} \n")
+            web_ent.delete(0, END)
+            passwd_ent.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -25,17 +62,17 @@ passwd_l.grid(column=0, row=4)
 # entries
 web_ent = Entry(width=35)
 web_ent.grid(column=1, row=2, columnspan=2)
+web_ent.focus()
 user_ent = Entry(width=35)
 user_ent.grid(column=1, row=3, columnspan=2)
+user_ent.insert(0, "smith@mail.com")
 passwd_ent = Entry(width=21)
 passwd_ent.grid(column=1, row=4)
 
 # buttons
-passwd_gen_b = Button(text="Generate Password")#, command=)
+passwd_gen_b = Button(text="Generate Password", command=passwd_gen)
 passwd_gen_b.grid(column=2, row=4)
-add_b = Button(text="Add", width=36)#, command=)
+add_b = Button(text="Add", width=36, command=add_entry)
 add_b.grid(column=1, row=5, columnspan=2)
-
-
 
 window.mainloop()
