80b379fd48ecfea36ae18c137aa8ecd0fd7ad12a
sarkis-k <sargis.leva@gmail.com>
2022-04-14 20:24:59 -0700

    added window and canvas added logo to the window

---
+++ 29_pswd_generator/main.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(height=200, width=200)
+logo = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo)
+canvas.grid(column=1, row=1)
+
+
+
+
+window.mainloop()
