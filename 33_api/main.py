8224fde73a3a6a53c82a743eefd048b873850460
sarkis-k <sargis.leva@gmail.com>
2022-04-26 01:12:47 -0700

    getting sunset time form api and linking it to iss location

--- 33_api/main.py
+++ 33_api/main.py
@@ -1,6 +1,6 @@
 import requests
 
-#ISS requests
+# ISS requests
 
 # response = requests.get(url="http://api.open-notify.org/iss-now.json")
 # response.raise_for_status()
@@ -11,32 +11,48 @@ import requests
 #
 # print(iss_pos)
 
-#Kanye West requests
+# Kanye West requests
 
-from tkinter import *
+# from tkinter import *
+#
+#
+# def get_quote():
+#     response = requests.get(url="http://api.kanye.rest")
+#     response.raise_for_status()
+#
+#     quote = response.json()["quote"]
+#     canvas.itemconfigure(quote_text, text=quote)
+#
+#
+# window = Tk()
+# window.title("Kanye Says...")
+# window.config(padx=50, pady=50)
+#
+# canvas = Canvas(width=300, height=414)
+# background_img = PhotoImage(file="background.png")
+# canvas.create_image(150, 207, image=background_img)
+# quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"),
+#                                 fill="white")
+# canvas.grid(row=0, column=0)
+#
+# kanye_img = PhotoImage(file="kanye.png")
+# kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+# kanye_button.grid(row=1, column=0)
+#
+# window.mainloop()
 
-def get_quote():
-    response = requests.get(url="http://api.kanye.rest")
-    response.raise_for_status()
 
-    quote = response.json()["quote"]
-    canvas.itemconfigure(quote_text, text=quote)
+# Getting
+parameters = {
+    "lat": 34.052235,
+    "lng": -118.243683,
+}
 
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
 
-window = Tk()
-window.title("Kanye Says...")
-window.config(padx=50, pady=50)
+data = response.json()
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
 
-canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
-canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
-canvas.grid(row=0, column=0)
-
-kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
-kanye_button.grid(row=1, column=0)
-
-
-
-window.mainloop()
+print(data)
