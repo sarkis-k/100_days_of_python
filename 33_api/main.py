59dd956d65f8112dd13e1abfd976006f85a0854b
sarkis-k <sargis.leva@gmail.com>
2022-04-26 16:42:46 -0700

    added functions to compare if iss is in are and if it's night time

--- 33_api/main.py
+++ 33_api/main.py
@@ -1,15 +1,59 @@
+import time
 import requests
+import datetime as dt
+
+def is_in_area():
+    # return true if iss is in the circle +-5 degree
+    if my_lat-5 <=iss_lat <= my_lat+5:
+        if my_long-5 <= iss_long <= my_long+5
+            return True
+    return False
+
+def is_night():
+    # return true if it's night sky in my place
+    if sunset <= time_now.hour <= sunrise
+        return True
+    return False
 
 # ISS requests
 
-# response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# response.raise_for_status()
-#
-# long = response.json()["iss_position"]["longitude"]
-# lat = response.json()["iss_position"]["latitude"]
-# iss_pos = (long, lat)
-#
-# print(iss_pos)
+iss_response = requests.get(url="http://api.open-notify.org/iss-now.json")
+iss_response.raise_for_status()
+
+iss_long = float(iss_response.json()["iss_position"]["longitude"])
+iss_lat = float(iss_response.json()["iss_position"]["latitude"])
+iss_pos = (iss_long, iss_lat)
+
+print(iss_pos)
+
+# ISS only visible at night. get the sunset from api and if iss is in your sky notify yourself
+
+my_long = -118.243683
+my_lat = 34.052235
+
+parameters = {
+    "lat": my_lat,
+    "lng": my_long,
+    "formatted": 0
+}
+
+sun_response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+sun_response.raise_for_status()
+
+data = sun_response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+
+time_now = dt.datetime.now()
+
+while True:
+    if is_night():
+        if is_in_area():
+            #open smtp and send an email
+            pass
+    time.sleep(60)
+
 
 # Kanye West requests
 
@@ -40,28 +84,3 @@ import requests
 # kanye_button.grid(row=1, column=0)
 #
 # window.mainloop()
-
-
-# ISS only visible at night. get the sunset from api and if iss is in your sky notify yourself
-
-import datetime as dt
-
-parameters = {
-    "lat": 34.052235,
-    "lng": -118.243683,
-    "formatted": 0
-}
-
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-
-data = response.json()
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
-
-
-print(sunset)
-print(sunrise)
-
-time_now = dt.datetime.now()
-
