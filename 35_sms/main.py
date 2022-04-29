3dea5729ddb4dc4f80ce0dc48b9cca6d3cc7ceaf
sarkis-k <sargis.leva@gmail.com>
2022-04-29 13:16:49 -0700

    implemented sms sending api

--- 35_sms/main.py
+++ 35_sms/main.py
@@ -1,13 +1,40 @@
 import requests
-from api_key import api_key
+from api_key import *
+from twilio.rest import Client
 
+
+account_sid = twilio_account_sid
+auth_token = twilio_auth_token
 
 api_url = "https://api.openweathermap.org/data/2.5/onecall"
 api_param = {
-    "lat": 34.052235,
-    "lon": -118.243683,
+    "lat": 42.824300,  # 34.052235,
+    "lon": -99.750080,  # -118.243683,
+    "exclude": "current,minutely,daily",
     "appid": api_key
 }
 
-# response = requests.get(url="")
-print(api_key)
+response = requests.get(url=api_url, params=api_param)
+response.raise_for_status()
+weather_data = response.json()
+
+will_rain = False
+
+for x in range(0, 12):
+    weather_condition = weather_data["hourly"][x]["weather"][0]["id"]
+    if weather_condition < 700:
+        will_rain = True
+
+if will_rain:
+    print("bring it")
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_="+19894814919",
+                     to="+18184349097"
+    )
+    print(message.status)
+
+
+
+
