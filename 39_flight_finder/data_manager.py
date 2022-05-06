fde75613208893df95e8c756f1c3d17983dee757
sarkis-k <sargis.leva@gmail.com>
2022-05-05 18:28:08 -0700

    implemented updating sheets

--- 39_flight_finder/data_manager.py
+++ 39_flight_finder/data_manager.py
@@ -1,20 +1,33 @@
 import requests
 from keys import *
-from flight_search import *
+# import flight_search
 
 
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    def get_cities(self):
-        response = requests.get(get_sheety_url)
+    def __int__(self):
+        self.sheet_data = {}
+
+    def get_data(self):
+        response = requests.get(sheety_url)
         response.raise_for_status()
-        data = response.json()["prices"]
-        cities = []
-        for x in data:
-            cities.append(x["city"])
+        self.sheet_data = response.json()["prices"]
+        return self.sheet_data
 
-        print(cities)
-        return cities
+    def update_data(self):
+        for city in self.sheet_data:
+            update_load = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{sheety_url}/{city['id']}",
+                json=update_load
+            )
+            response.raise_for_status()
+            print(response.text)
 
-    def post_iata(self):
-        response = requests.put(put_sheety_iata)
+
+
+
--- 39_flight_finder/main.py
+++ 39_flight_finder/main.py
@@ -4,6 +4,17 @@ from flight_data import *
 from flight_search import *
 from notification_manager import *
 import requests
+from pprint import pprint
 
-FlightSearch()
-DataManager()
+data_manager = DataManager()
+sheety_data = data_manager.get_data()
+
+if sheety_data[0]["iataCode"] == "":
+    flight_search = FlightSearch()
+    for row in sheety_data:
+        row["iataCode"] = flight_search.iata_search(row["city"])
+    print(f"sheet_data:\n {sheety_data}")
+
+    data_manager.sheet_data = sheety_data
+    data_manager.update_data()
+
