dddc68e4fa4438cacb6efc06ac156fb7cb659bdc
sarkis-k <sargis.leva@gmail.com>
2022-05-10 19:31:33 -0700

    scraping yc website and printing the most upvote news

--- 45_scrapping/main.py
+++ 45_scrapping/main.py
@@ -1,9 +1,23 @@
 from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://news.ycombinator.com/")
+yc_page = response.text
+
+soup = BeautifulSoup(yc_page, "html.parser")
+title = soup.title
+
+articles = soup.find_all(name="a", class_="titlelink")
+articles_texts = []
+articles_links = []
+for article_tag in articles:
+    articles_texts.append(article_tag.getText())
+    articles_links.append(article_tag.get("href"))
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+
+index = article_upvotes.index(max(article_upvotes))
+print(articles_texts[index]+articles_links[index])
 
 
-with open("website.html") as file:
-    contents = file.read()
 
-soup = BeautifulSoup(contents, "html.parser")
-all_a = soup.find_all(name="li")
-print(all_a)
+
