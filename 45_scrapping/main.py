5a927505021c4e01de718030aa772a48acaf49da
sarkis-k <sargis.leva@gmail.com>
2022-05-10 18:28:09 -0700

    practice bs4 commands

--- 45_scrapping/main.py
+++ 45_scrapping/main.py
@@ -4,4 +4,6 @@ from bs4 import BeautifulSoup
 with open("website.html") as file:
     contents = file.read()
 
-
+soup = BeautifulSoup(contents, "html.parser")
+all_a = soup.find_all(name="li")
+print(all_a)
